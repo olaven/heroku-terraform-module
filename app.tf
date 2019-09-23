d7c83c833c60d1f59131dbde86acb0404b506424
olaven <olav@sundfoer.com>
2019-09-23 11:30:59 +0200

    variable syntax now compatible < 0.12

--- app.tf
+++ app.tf
@@ -1,18 +1,18 @@
 # Creating apps:
 resource "heroku_app" "test" {
   name = "${var.name}-test"
-  region = var.region
-  buildpacks = var.buildpacks
+  region = "${var.region}"
+  buildpacks = "${var.buildpacks}"
 }
 
 resource "heroku_app" "staging" {
   name = "${var.name}-staging"
-  region = var.region
-  buildpacks = var.buildpacks
+  region = "${var.region}"
+  buildpacks = "${var.buildpacks}"
 }
 
 resource "heroku_app" "production" {
   name = "${var.name}-production"
-  region = var.region
-  buildpacks = var.buildpacks
+  region = "${var.region}"
+  buildpacks = "${var.buildpacks}"
 }
--- pipeline.tf
+++ pipeline.tf
@@ -4,13 +4,13 @@ resource "heroku_pipeline" "deploy" {
 }
 
 resource "heroku_pipeline_coupling" "staging" {
-  app = heroku_app.staging.name
-  pipeline = heroku_pipeline.deploy.id
+  app = "${heroku_app.staging.name}"
+  pipeline = "${heroku_pipeline.deploy.id}"
   stage = "staging"
 }
 
 resource "heroku_pipeline_coupling" "production" {
-  app = heroku_app.production.name
-  pipeline = heroku_pipeline.deploy.id
+  app = "${heroku_app.production.name}"
+  pipeline = "${heroku_pipeline.deploy.id}"
   stage = "production"
 }
