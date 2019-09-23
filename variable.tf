e38f394ac64ebf99cd4a395e4f28c9ae81d79f7d
olaven <olav@sundfoer.com>
2019-09-23 11:25:55 +0200

    added types

--- variable.tf
+++ variable.tf
@@ -1,10 +1,12 @@
 
 variable "name" {
   description = "Base name for the Heroku app"
+  type = "string"
 }
 
 variable "buildpacks" {
   description = "Array of heroku buildpacks"
+  type = "list"
 }
 
 variable "region" {
